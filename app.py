dead3e7ca0bd1c7e8b60088e0265270ff6ddaabb
Kevin Malazarte <Kevin.Malazarte@gmail.com>
2020-02-24 14:11:12 -0500

    fixed class syntax, run test function, and called it

--- app.py
+++ app.py
@@ -14,3 +14,12 @@ questions = [
 
 
 def run_test(questions):
+    score = 0
+    for question in questions:
+        answer = input(question.prompt)
+        if answer == question.answer:
+            score += 1
+    print("You got" + str(score) + "/" + str(len(questions + "Correct")))
+
+
+run_test(questions)
